ae966b076c0f4772a41ae8b8dfcbed986eb8984d
ThirdHouseCaptain <thirdhousecaptain@users.noreply.github.com>
2020-09-28 15:47:36 +0530

    Calibrun Mirco Migration

---
+++ ViewModels/MainLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace bug_tracker.ViewModels
+{
+    public class MainLoginViewModel : Screen
+    {
+    }
+}
